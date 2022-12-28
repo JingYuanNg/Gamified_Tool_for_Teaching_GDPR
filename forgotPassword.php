1c5b76dd59274a421f77f0503df7e8120b46c56a
JingYuanNg <ngjingyuan020115@gmail.com>
2022-12-28 14:08:40 +0000

    Update forgotPassword.php
    - take down password for security purpose

--- forgotPassword.php
+++ forgotPassword.php
@@ -89,7 +89,7 @@
                            $mail->Host = 'smtp.gmail.com';
                            $mail->SMTPAuth = true;
                            $mail->Username = 'developerInshield@gmail.com';
-                           $mail->Password = '34567890sDFGHJKL!!';
+                           $mail->Password = ''; //password is not uploaded to github for security purpose
                            $mail->SMTPSecure = 'tls';
                            $mail->Port = 587;
 
